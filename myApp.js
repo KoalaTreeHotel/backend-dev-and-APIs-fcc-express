3926c44b24a34e63ff57fc06225e264302ea37d4
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-07-09 21:06:36 +0100

    express progression

--- myApp.js
+++ myApp.js
@@ -1,39 +1,17 @@
 let express = require('express');
 let app = express();
 
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
 
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+// Assets at the / public route
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+console.log("dirname = " + __dirname);
+module.exports = app;
