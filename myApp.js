7bf62e361cb909853d969f431313097b55c95013
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-07-12 18:33:02 +0100

    completed basic node and express section

--- myApp.js
+++ myApp.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
+// Body parser middleware for POST
+let bodyParser = require("body-parser");
 
 let express = require("express");
 let app = express();
 
 // const path = require("path");
 // const __dirname = path.resolve(path.dirname(""));
+
+// Used for POST, as usual, must mount the middleware
+// must be mounted before all the routes that depend on it
+// User data POSTed in body, not in URL
+let middlewareParse = bodyParser.urlencoded({ extended: false });
+app.use(middlewareParse);
 
 // Mount the logger middleware
 app.use(function middleware(req, res, next) {
@@ -45,7 +53,7 @@ app.get("/json", (req, res) => {
     });
 });
 
-// Chaining middleware together. 
+// Chaining middleware together.
 // Endpoint "/now" returns the current JSON time.
 app.get("/now", (req, res, next) => {
     req.time = new Date().toString();
@@ -55,11 +63,35 @@ app.get("/now", (req, res, next) => {
 });
 
 // Getting a route parameter input from the user / client
+// Using the URL to pass data to the server (after the route
+// path)
 // eg: 127.0.0.1/tree/echo  ->  {"echo":"tree"}
 app.get("/:word/echo", (req, res) =>
     res.send({ "echo": req.params.word }));
 
+// Get Query Parameter Input from the Client via URL
+// (after the route path, add in data)
+// EG: 127.0.0.1:3000/name?first=FIRSTNAME&last=LASTNAME
+// EG: http://127.0.0.1:3000/name?first=John&last=Smith
+app.route("/name").get((req, res) =>
+    res.send({ "name": `${req.query.first} ${req.query.last}` }));
 
+// Use body-parser to Parse POST Requests
+// Instead of using the URL (parameters after the route path),
+// We can also POST data. POST is the default method to send
+// data with HTML forms.
+// In REST convention, POST is used to send data to create new
+// items in a database.
+// In these kind of requests, the data doesn't appear in the URL,
+// it is hidden in the request body (beware, hidden, but NOT private)
+//
+// We're getting the data from the HTML form in index.html
+// (not from the URL). This form has an action of "/name"
+// HTML form action specifies the URL to send the form-data
+app.route("/name").post((req, res) =>
+    res.send({ "name": `${req.body.first} ${req.body.last}` }));
+
+// Get Data from POST Requests
 
 
 
